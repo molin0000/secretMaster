24d8e6d724b10e98ffc9629d61af3083d9db7b7c
Mo Lin <molin@wanchain.org>
2020-03-17 20:29:53 +0800

    add

---
+++ pet/pet.go
@@ -0,0 +1,18 @@
+package pet
+
+type PetStore struct {
+}
+
+type Pet struct {
+	Nick    string
+	Class   string
+	Star    uint64
+	Level   uint64
+	Money   uint64
+	Exp     uint64
+	HP      uint64
+	Attack  uint64
+	Defense uint64
+	Speed   uint64
+	Charm   uint64
+}
