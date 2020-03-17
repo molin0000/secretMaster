d914b29eff713cf70fd5ceeb7b84422d0755429c
Mo Lin <molin@wanchain.org>
2020-03-18 00:35:17 +0800

    update

--- pet/pet.go
+++ pet/pet.go
@@ -4,15 +4,20 @@ type PetStore struct {
 }
 
 type Pet struct {
-	Nick    string
-	Class   string
-	Star    uint64
-	Level   uint64
-	Money   uint64
-	Exp     uint64
-	HP      uint64
-	Attack  uint64
-	Defense uint64
-	Speed   uint64
-	Charm   uint64
+	From       string
+	Class      string
+	Price      string
+	Commit     string
+	Skill      string
+	GrowthType uint64
+	Nick       string
+	Star       uint64
+	Level      uint64
+	Money      uint64
+	Exp        uint64
+	HP         uint64
+	Attack     uint64
+	Defense    uint64
+	Speed      uint64
+	Charm      uint64
 }
