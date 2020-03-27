19da744d27cdb8f9b28744a4cc01210b61258915
lolieatapple <lolieatapple@gmail.com>
2020-03-27 23:02:13 +0800

    update pro compile.

--- b.sh
+++ b.sh
@@ -6,3 +6,8 @@ mkdir -p ~/coolq/dev/me.cqp.molin.secretmaster
 cp app.dll ~/coolq/dev/me.cqp.molin.secretmaster/
 cp app.json ~/coolq/dev/me.cqp.molin.secretmaster/
 
+rm -rf ~/coolqPro/dev/me.cqp.molin.secretmaster
+mkdir -p ~/coolqPro/dev/me.cqp.molin.secretmaster
+cp app.dll ~/coolqPro/dev/me.cqp.molin.secretmaster/
+cp app.json ~/coolqPro/dev/me.cqp.molin.secretmaster/
+
