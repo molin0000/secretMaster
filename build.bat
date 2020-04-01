77ba62a13f4a0c37d71987e522bba604d535e703
Mariano-starter <59152140+Mariano-starter@users.noreply.github.com>
2020-04-01 23:18:19 +0800

    增加了序列名称

--- build.bat
+++ build.bat
@@ -6,16 +6,17 @@ go build github.com/Tnze/CoolQ-Golang-SDK/tools/cqcfg
 go generate
 IF ERRORLEVEL 1 pause
 
-echo Setting env vars
-SET CGO_LDFLAGS=-Wl,--kill-at
-SET CGO_ENABLED=1
-SET GOOS=windows
-SET GOARCH=386
-SET GOPROXY=https://goproxy.cn
+docker start -a build-gocqplg
+::echo Setting env vars
+::SET CGO_LDFLAGS=-Wl,--kill-at
+::SET CGO_ENABLED=1
+::SET GOOS=windows
+::SET GOARCH=386
+::SET GOPROXY=https://goproxy.cn
 
-echo Building app.dll
-go build -ldflags "-s -w" -buildmode=c-shared -o app.dll
-IF ERRORLEVEL 1 pause
+::echo Building app.dll
+::go build -ldflags "-s -w" -buildmode=c-shared -o app.dll
+::IF ERRORLEVEL 1 pause
 
 :: Copy app.dll amd app.json
 ::SET DevDir=D:\序列战争版本更新\酷Q\coolq\dev\me.cqp.molin.secretmaster\
